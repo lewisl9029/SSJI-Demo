9f3882e6da4a77c62c29b46959467ac9eef9dfd1
Lewis Liu <develop@lewisl.net>
2014-07-27 20:55:01 -0400

    -scaffolded angular-fullstack app

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ssjidemoApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
